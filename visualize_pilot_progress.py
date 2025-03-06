3f474ffac7e8dd9542457b9738e1a7906edf74ed
Benjamin Fankhauser <benjamin.fankhauser@students.unibe.ch>
2025-03-06 09:19:04 +0100

    Pilot progress 3d Update

--- visualize_pilot_progress.py
+++ visualize_pilot_progress.py
@@ -1,44 +1,171 @@
+import datetime
+import utm
+
 import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_pilot(df, name):
+from mpl_toolkits.mplot3d import Axes3D
+
+def as_seconds(t):
+    return t.hour*3600 + t.minute*60 + t.second
+
+def height_compensation(delta_altitude, delta_distance, delta_time):
+    if delta_altitude < 0:
+        # 2m height compensation:
+        mcgreedy_climb = 2.0
+        extra_time = -1*delta_altitude / mcgreedy_climb
+        compensated_speed = delta_distance / (delta_time+extra_time)
+    else:
+        # "virtually glide 60km/h@8"
+        glide_speed = 60.0/3.6 # in m/s
+        glide_ratio = 7
+        glide_distance = delta_altitude*glide_ratio
+        extra_time = glide_distance/glide_speed
+        compensated_speed = (delta_distance+glide_distance)/(delta_time+extra_time)
+    return extra_time, compensated_speed
+
+def height_compensation_speed(delta_altitude, delta_distance, delta_time):
+    _, speed = height_compensation(delta_altitude, delta_distance, delta_time)
+    return speed
+
+def process_pilot(filename, airstart, start, end, name):
+    df = pd.read_csv(filename)
+    # convert time
+    df['time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x, '%H:%M:%S').time())
+    df['seconds'] = df['time'].apply(lambda t: as_seconds(t) - as_seconds(airstart))
+    
     # filter by airstart
     df = df[df['airstart'] == True]
+    #df = crop_time(df, start, end)
+    df = crop_distance(df, start, end)
+
+    # convert x,y coordinates:
+    xs, ys, _, _ = utm.from_latlon(df['lat'].values, df['lon'].values)
+    df['x'] = xs
+    df['y'] = ys
+
+    # Resample in 100m of distance    
+    df['distance_bin'] = (df['distance'] // 10) * 10 # Round every 10m
+    df = df.groupby('distance_bin').first().reset_index()
+
+    # set index distance
+    df.set_index('distance_bin', inplace=True)
+    df.sort_index(ascending=False, inplace=True)
+
+    # Compute statistics    
+    first_row = df.iloc[0]    
+    last_row = df.iloc[-1]
+    delta_time = last_row['seconds'] - first_row['seconds']
+    delta_gps_alt = last_row['gps_alt'] - first_row['gps_alt']
+    delta_distance = first_row['distance'] - last_row['distance']
+
+    climb = delta_gps_alt / delta_time
+    speed = delta_distance / delta_time
+
+    extra_time, compensated_speed = height_compensation(delta_gps_alt, delta_distance, delta_time)
+
+
+    print(name, 'time:', delta_time, 'altitude', delta_gps_alt, 'distance', delta_distance, 'climb', climb, 'speed', speed*3.6, 'extra_time', extra_time, 'compensated_speed', compensated_speed*3.6)
+
+
+    # compute diffs
+    periods=100 # 1=10m
+    df['delta_seconds'] = df['seconds'].diff(periods=periods)    
+    df['delta_gps_alt'] = df['gps_alt'].diff(periods=periods)    
+    df['delta_distance'] = df['distance'].diff(periods=periods).apply(lambda x: -x)
+    
+    df['climb'] = df['delta_gps_alt'] / df['delta_seconds']
+    df['speed'] = (df['delta_distance'] / df['delta_seconds'])
+    
+    df['compensated_speed'] = df.apply(lambda row: height_compensation_speed(row['delta_gps_alt'], row['delta_distance'], row['delta_seconds']), axis=1)
+    
+    #print(df)
+
+    #df[df['speed'] > 25.] = 25.
+
+    #plot_pilot(df, name)
+    plot_pilot_3d(df, name)
+
+def crop_time(df, start, end):
+    df = df[df['time'] >= start]
+    df = df[df['time'] < end]
+    return df
+
+def crop_distance(df, start, end):
+    df = df[df['distance'] <= start]
+    df = df[df['distance'] > end]
+    return df
+
+def plot_pilot_3d(df, name):
+
+    #sc = ax.scatter(df['x'], df['y'], df['gps_alt'], marker='o', label=name) 
+    sc = ax.scatter(df['x'], df['y'], df['gps_alt'], c=df['distance'], cmap='viridis', marker='o', label=name) 
+                    
+    
+
+def plot_pilot(df, name):    
 
     # Reduce the points
-    reduction = 50 # aggregate this many rows
+    #reduction = 10 # aggregate this many rows
     #df = df[df.index % reduction == 0]
-    df['gps_alt'] = df['gps_alt']/1700*30000
-    df = df.groupby(df.index // reduction).agg({'time': 'first', 'distance': 'mean', 'gps_alt': 'mean'})
+    #df['gps_alt'] = df['gps_alt']/1700*30000
     
-    df['time'] = np.arange(len(df))
-    df = df[df['time'] < 70.]
+    value = 'gps_alt'
+    #df = df.groupby(df.index // reduction).agg({'time': 'first', 'seconds': 'first', 'distance': 'mean', 'gps_alt': 'mean', 'speed': 'mean', 'climb': 'mean'})
+    
+    #df['time'] = np.arange(len(df))
+    #df = df[df['time'] < 70.]
 
-    sns.lineplot(data=df, x='time', y='distance', marker='o', label=name)
-    sns.lineplot(data=df, x='time', y='gps_alt', label=f'{name} GPS Alt')
+    #sns.lineplot(data=df, x='seconds', y='distance', marker='o', label=name)
+    #sns.lineplot(data=df, x='seconds', y='gps_alt', label=f'{name} GPS Alt')
+
+    #sns.lineplot(data=df, x=df.index, y=value, marker='o', label=f'{name} {value}')
 
 # Plot using Seaborn
-sns.set_theme(style="darkgrid")  # Optional for styling
-plt.figure(figsize=(8, 5))
+#sns.set_theme(style="darkgrid")  # Optional for styling
+#plt.figure(figsize=(8, 5))
+
+# 3d Plot:
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
 
 #sns.scatterplot(data=df, x='time', y='distance', alpha=0.3, s=10)
 # load dumped file:
-df_benjamin = pd.read_csv('dump/benjamin.csv')
-plot_pilot(df_benjamin, 'Benjamin')
-df_roger = pd.read_csv('dump/roger.csv')
-plot_pilot(df_roger, 'Roger')
-df_patrick = pd.read_csv('dump/patrick.csv')
-plot_pilot(df_patrick, 'Patrick')
+airstart = datetime.time(11, 35)
+crop_start = datetime.time(11, 30)
+#crop_end = datetime.time(13, 30)
+crop_end = datetime.time(11, 50)
+
+d_start = 10000
+d_end = 00000
+
+process_pilot('dump/benjamin.csv', airstart, d_start, d_end, 'Benjamin')
+process_pilot('dump/patrick.csv', airstart, d_start, d_end, 'Patrick')
+process_pilot('dump/roger.csv', airstart, d_start, d_end, 'Roger')
+
 
 #plt.xticks(ticks=range(0, len(df_benjamin), 10), rotation=45)
 
-# Show the plot
+# Finish 3d Plot
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Altitude')
+ax.set_title('3D Scatter Plot Colored by Distance')
+ax.view_init(elev=25, azim=135)
+
+#cbar = plt.colorbar(sc, ax=ax, shrink=0.6, aspect=10, pad=0.1)
+#cbar.set_label('Distance (m)')
+
+# Show the plot (Seaborn)
 plt.legend()
-plt.xlabel("Time")
-plt.ylabel("Distance")
-plt.title("Time vs Distance")
+#plt.gca().invert_xaxis() # flip towards 0
+#plt.xlabel("Distance")
+#plt.ylabel("Value")
+#plt.title("Distance vs Value")
+
+
 plt.show()
 
 
